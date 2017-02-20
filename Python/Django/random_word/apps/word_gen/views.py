659ed8619a0d41700dbee06073bb0b0694c94992
Dalia <dalia.icedo@gmail.com>
2017-02-20 02:14:32 -0800

    new projects 2/20

--- Python/Django/random_word/apps/word_gen/views.py
+++ Python/Django/random_word/apps/word_gen/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import string
+import random
 
+#CONTROLLER
 # Create your views here.
+def index(request):
+    print('8'*100)
+    return render(request, 'word_gen/index.html')
+
+def word_gen(request):
+    if request.method == "POST":
+        characters = ['0', '1', '2', '3', '4', '4', '5', '6',
+                      '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                      'I', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                      'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        generated_word = ''
+        for i in range(14):
+            letter = random.choice(characters)
+            generated_word = generated_word + letter
+        request.session['random_word'] = generated_word
+    return redirect('/')
