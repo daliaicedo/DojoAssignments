ee178f572abb09b74ddc4cd4edf254f34aa8a99c
Dalia <dalia.icedo@gmail.com>
2017-02-23 00:40:08 -0800

    finally finished login and registration

--- Python/Django/login_reg/apps/login_registration/models.py
+++ Python/Django/login_reg/apps/login_registration/models.py
@@ -1,93 +1,103 @@
 from __future__ import unicode_literals
-
 from django.db import models
+import bcrypt
+import re
+import time
+#just import everything you need ok
 
-# Create your models here.
-class Users(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.TextField(max_length=100)
-    email = models.TextField(max_length=100)
-    dob = models.DateTimeField()
-    pw_hash = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    def __str__(self):
-    return self.first_name + " " + self.last_name
-
-
-
+#validation stuff
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+NAME_REGEX = re.compile(r'^[a-zA-Z]+$')
 
 
 # Create your models here.
 class UserManager(models.Manager):
-    def validate(self, postData):
-        errors = []     # array of error messages
-        if postData["first_name"] == "First Name" or len(postData["first_name"]) == 0:
-            # if first name has not been entered
-            errors.append("Please enter a first name.")
-        elif len(postData["first_name"]) < 2:
-            errors.append("First name must be between 2-45 characters.")
-        elif not re.search(r'^[A-Za-z]+$', postData["first_name"]):
-            errors.append("First name must be letters only.")
-        if postData["last_name"] == "Last Name" or len(postData["last_name"]) == 0:
-            errors.append("Please enter a last name.")
-        elif len(postData["last_name"]) < 2:
-            errors.append("Last name must be between 2-45 characters.")
-        elif not re.search(r'^[A-Za-z]+$', postData["last_name"]):
-            errors.append("Last name must be letters only.")
-        if postData["email"] == "example@email.com" or len(postData["email"]) == 0:
-            errors.append("Please enter an email address.")
-        elif not re.search(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+.[a-zA-Z]+$', postData["email"]):
-            errors.append("Invalid email address.")
-        elif len(User.objects.filter(email=postData["email"])) > 0:
-            # if list of users w/ this email is empty
-            errors.append("Email address is already registered.")
-        try:
-            dob = datetime.strptime(postData["dob"], "%m/%d/%Y")
-        except ValueError:
-            errors.append("Invalid date of birth entered. Use M/D/YYYY format.")
-        else:
-            if datetime.now() < dob:
-                errors.append("Future date of birth entered.")
-        if len(postData["password"]) < 8:
-            errors.append("Password must be 8 or more characters.")
-        if postData["confirm"] != postData["password"]:
-            errors.append("Passwords do not match.")
-        if len(errors) == 0:
-            try:
-                user = User.objects.create(first_name=postData["first_name"], last_name=postData["last_name"],email=postData["email"],dob=dob,pw_hash=bcrypt.hashpw(postData["password"].encode(), bcrypt.gensalt()))
-            except OverflowError:
-                # Encountered an error when attempting to input 1/1/1900 as date of birth
-                errors.append("Invalid date of birth.")
-                return (False, errors)
-            return (True, user)
-            # returns (success code, user object)
-        else:
-            return (False, errors)
-            # returns (failure, error list)
+    def register(self, **kwargs):
+        print "** now using user manager **"
+        print "** registration in process! **"
+        status = {}
+        messages = []
+        f_name = kwargs['first_name'][0]
+        l_name = kwargs['last_name'][0]
+        email = kwargs['email'][0]
+        password = kwargs['password'][0]
+        confirm_password = kwargs['confirm_password'][0]
+        birthday = kwargs['birthday'][0]
+        print birthday
+        #checking first name
+        if len(f_name) <= 1:
+            messages.append('First name has to be at least 2 characters.')
+        elif not NAME_REGEX.match(f_name):
+            messages.append('First name can only contain letters.')
+        #checking last name
+        if len(l_name) <= 1:
+            messages.append('Last name has to be at least 2 characters.')
+        elif not NAME_REGEX.match(l_name):
+            messages.append('Last name can only contain letters.')
+        #checking email
+        if len(email) < 1:
+            messages.append('E-mail is required.')
+        elif not EMAIL_REGEX.match(email):
+            messages.append('Invalid e-mail format.')
+        elif len(User.userManager.filter(email=email)) > 0:
+            messages.append('User already exists.')
+        #checking password
+        if len(password) < 8:
+            messages.append('Password should be at least 8 characters.')
+        elif password != confirm_password:
+            messages.append('Password fields do not match.')
+        #checking birthday
+        if not re.search(r'^[0-9][0-9][0-9][0-9][\-][0-9][0-9][\-][0-9][0-9]', birthday):
+            messages.append('Invalid birthday format. Should be YYYY-MM-DD format.')
+        elif birthday > time.strftime("%Y-%m-%d"):
+            messages.append('Invalid birthday! Need to be from the past.')
 
-    def authenticate(self, postData):
-        if "email" in postData and "password" in postData:
-            try:
-                user = User.objects.get(email=postData["email"])
-            except User.DoesNotExist:
-                return (False, "Invalid email/password combination.")
-            pw_match = bcrypt.hashpw(postData['password'].encode(),user.pw_hash.encode())
-            if pw_match:
-                return (True, user)
-            else:
-                return (False, "Invalid email/password combination.")
+        if not messages:
+            valid = True
+            messages.append('Thank you for registering! Please sign in.')
+            pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+            User.userManager.create(first_name = f_name, last_name = l_name, email = email, password = pw_hash, birthday = birthday)
         else:
-            return (False, "Please enter login info.")
+            valid = False
 
+        status.update({'valid': valid, 'messages': messages})
+        return status
+
+    def login(self, **kwargs):
+        print "** Using user manager **"
+        print "** Checking login **"
+        status = {}
+        messages = []
+        email = kwargs['email'][0]
+        password = kwargs['password'][0]
+        print email, password
+
+        if len(email) < 1 or len(password) < 1:
+            messages.append("Login fields cannot be blank.")
+        else:
+            userinfo = User.userManager.filter(email=email)
+            if not userinfo:
+                messages.append("Unable to find user. Please register.")
+            elif not bcrypt.checkpw(password.encode(), userinfo[0].password.encode()):
+                messages.append("Incorrect password.")
+
+        if not messages:
+            valid = True
+            status.update({'user_id': userinfo[0].id})
+        else:
+            valid = False
+            status.update({'messages': messages})
+        status.update({'valid': valid})
+        return status
 
 class User(models.Model):
-    first_name = models.CharField(max_length=45)
-    last_name = models.CharField(max_length=45)
-    email = models.CharField(max_length=45)
-    dob = models.DateTimeField()
-    pw_hash = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.TextField(max_length=100)
+    email = models.TextField(max_length=100)
+    birthday = models.DateField(auto_now = False, auto_now_add = False)
+    password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    objects = UserManager()
+    # def __str__(self):
+    # return self.first_name + " " + self.last_name
+    userManager = UserManager()
--- Python/Django/login_reg/apps/login_registration/urls.py
+++ Python/Django/login_reg/apps/login_registration/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^registration$', views.registration),
-    url(r'^success$', views.success)
+    url(r'^login$', views.login),
+    url(r'^register$', views.register),
+    url(r'^success$', views.welcome),
+    url(r'^logout$', views.logout),
+    url(r'^delete$', views.delete),
+    url(r'^.+$', views.any)
 ]
