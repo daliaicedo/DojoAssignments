dae2c0b052a139000e6783465aa7c61950b52a89
Dalia <dalia.icedo@gmail.com>
2017-03-10 01:17:02 -0800

    iOS projects added

--- iOS/tipster/tipster/ViewController.swift
+++ iOS/tipster/tipster/ViewController.swift
@@ -20,6 +20,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var billAmount: UILabel!
+    @IBOutlet weak var tax1: UILabel!
+    @IBOutlet weak var tax1Amount: UILabel!
+    @IBOutlet weak var total1: UILabel!
+    @IBOutlet weak var tax2: UILabel!
+    @IBOutlet weak var tax2Amount: UILabel!
+    @IBOutlet weak var total2: UILabel!
+    @IBOutlet weak var tax3: UILabel!
+    @IBOutlet weak var tax3Amount: UILabel!
+    @IBOutlet weak var total3: UILabel!
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        if billAmount.text == "0" {
+            billAmount.text = String(sender.tag)
+        }
+        else if billAmount.text != "0" && sender.tag < 10{
+            var billTotal = billAmount.text
+            billAmount.text = billTotal! + String(sender.tag)
+        }
+        if billAmount.text == "0" && sender.tag == 11 {
+            billAmount.text = "0."
+        }
+        else if sender.tag == 11 {
+            var billTotal = billAmount.text
+            billAmount.text = billTotal! + "."
+        }
+        else if sender.tag == 12 {
+            billAmount.text = "0"
+        }
+        
+        
+    }
+    
 }
-
